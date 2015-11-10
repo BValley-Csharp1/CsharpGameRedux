24c17f09510fd9d915acd2eafda1e2bc9118a59d
Lou <lmcquaide@my.bridgevalley.edu>
2015-11-10 11:19:57 -0500

    changes to player todo: eliminate enum for better factoring

--- Game/PlayerClass.cs
+++ Game/PlayerClass.cs
@@ -11,13 +11,34 @@ namespace Game
         int moxie;
         int aggression;
         int humor;
-        enum charclass
+        public enum charclass
         {
             fighter, frat, lover, gentleman, joker, klutz
         }
         public Player(int c_class)
         {
+            charclass playerClass = new charclass();
             
+            case (c_class == 1):
+                playerClass = charclass.fighter;
+                break;
+
+            case (c_class == 2):
+                playerClass = charclass.frat;
+                break;
+            case (c_class == 3):
+                playerClass = charclass.lover;
+                break;
+            case (c_class == 4):
+                playerClass = charclass.gentleman;
+                break;
+            case (c_class == 5):
+                playerClass = charclass.joker;
+                break;
+            case (c_class == 6):
+                playerClass = charclass.klutz;
+                break;
+
         }
        
     }
