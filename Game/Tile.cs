7ce32bd2b1dc0bfe9777f1f9882cc6ff2d2f5597
juliannahanson <juliannahanson@outlook.com>
2015-11-11 10:53:02 -0500

    Reworking placement and creation of board obects, implementing properties that will allow NPC movement.

--- Game/Tile.cs
+++ Game/Tile.cs
@@ -10,10 +10,15 @@ namespace Game
     {
         public string symbol{ get; set;}
         public string originalSymbol { get; set; }
+
         public ConsoleColor color { get; set; }
         public ConsoleColor originalColor { get; set; }
+
+        //Used to keep track of player's location on the board
         public bool playerHere { get; set; }
-        public bool stairsHere { get; set; }
+
+        //Used for setting elements within the bar
+        public bool isOccupied { get; set; }
 
         //Checks to see if player can move past the tile
         public bool isPassable { get; set; }
